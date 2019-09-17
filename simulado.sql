6d5a11869bbd369db55dff8431b7924b52343f04
Vinicyus Macedo <7549205+vinicyusmacedo@users.noreply.github.com>
2019-09-16 21:48:51 -0300

    Added stored procedures

---
+++ simulado.sql
@@ -0,0 +1,47 @@
+-- Simulado 
+-- Criar uma stored procedure usando cursor explícito
+
+-- 8. Obter uma tabela que contém três colunas. 
+-- Na primeira coluna aparece o nome de cada disciplina que possui pré-requisito, 
+-- na segunda coluna aparece o nome de cada um de seus pré-requisitos 
+-- e a terceira contém o nível de pré-requisito. 
+-- Nível 1 significa que trata-se de um pré-requisito imediato da disciplina, 
+-- nível 2 significa que trata-se de um pré-requisito de um pré-requisito da disciplina, e assim por diante. 
+-- Limitar a consulta para três níveis. (DICA USAR UNION ALL)
+
+SELECT Disciplina.NomeDisc, DiscPre.NomeDisc, 1 AS Nivel
+FROM Disciplina NATURAL JOIN PreReq
+JOIN Disciplina AS DiscPre
+ON PreReq.CodDeptoPreReq = DiscPre.CodDepto
+AND PreReq.NumDiscPreReq = DiscPre.NumDisc
+
+UNION ALL
+
+SELECT Disciplina.NomeDisc, DiscPre.NomeDisc, 2 AS Nivel
+FROM (
+    (Disciplina NATURAL JOIN PreReq)
+    JOIN PreReq AS PrePreReq
+    ON PreReq.CodDeptoPreReq = PrePreReq.CodDepto
+    AND PreReq.NumDiscPreReq = PrePreReq.NumDisc
+)
+JOIN Disciplina AS DiscPre
+ON PrePreReq.CodDeptoPreReq = DiscPre.CodDepto
+AND PrePreReq.NumDiscPreReq = DiscPre.NumDisc
+
+UNION ALL
+
+SELECT Disciplina.NomeDisc, DiscPre.NomeDisc, 3 AS Nivel
+FROM (
+    (
+        (Disciplina NATURAL JOIN PreReq)
+        JOIN PreReq AS PrePreReq
+        ON PreReq.CodDeptoPreReq = PrePreReq.CodDepto
+        AND PreReq.NumDiscPreReq = PrePreReq.NumDisc
+    )
+    JOIN PreReq AS PrePrePreReq
+    ON PrePreReq.CodDeptoPreReq = PrePrePreReq.CodDepto
+    AND PrePreReq.NumDiscPreReq = PrePrePreReq.NumDisc
+)
+JOIN Disciplina AS DiscPre
+ON PrePrePreReq.CodDeptoPreReq = DiscPre.CodDepto
+AND PrePrePreReq.NumDiscPreReq = DiscPre.NumDisc;
---
+++ stored-procedures.sql
@@ -0,0 +1,50 @@
+-- 8. Obter uma tabela que contém três colunas. 
+-- Na primeira coluna aparece o nome de cada disciplina que possui pré-requisito, 
+-- na segunda coluna aparece o nome de cada um de seus pré-requisitos 
+-- e a terceira contém o nível de pré-requisito. 
+-- Nível 1 significa que trata-se de um pré-requisito imediato da disciplina, 
+-- nível 2 significa que trata-se de um pré-requisito de um pré-requisito da disciplina, e assim por diante. 
+-- Limitar a consulta para três níveis. (DICA USAR UNION ALL)
+
+drop table PreReqList;
+drop procedure PreReqProc;
+
+CREATE TABLE IF NOT EXISTS PreReqList (
+    NomeDisc VARCHAR(25),
+    NomeDiscPreReq VARCHAR(25),
+    Nivel INT
+);
+
+DELIMITER //
+
+CREATE PROCEDURE PreReqProc()
+BEGIN
+    DECLARE Done BOOLEAN DEFAULT FALSE;
+    DECLARE Nivel INT DEFAULT 1;
+    DECLARE NomeDisc, NomeDiscPreReq VARCHAR(25);
+    DECLARE cur CURSOR FOR 
+        SELECT Disciplina.NomeDisc, DiscPre.NomeDisc, Nivel FROM PreReq
+        NATURAL JOIN Disciplina
+        JOIN Disciplina AS DiscPre
+        WHERE PreReq.NumDiscPreReq = DiscPre.NumDisc
+        AND PreReq.CodDeptoPreReq = DiscPre.CodDepto;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET Done = TRUE;
+
+    OPEN cur;
+
+    fetch_loop: LOOP
+        FETCH cur INTO NomeDisc, NomeDiscPreReq, Nivel;
+        IF Done THEN
+            LEAVE fetch_loop;
+        END IF;
+        INSERT INTO PreReqList VALUES (NomeDisc, NomeDiscPreReq, Nivel);
+        SET Nivel = Nivel + 1;
+    END LOOP;
+    CLOSE cur;
+END//
+
+DELIMITER ;
+
+CALL PreReqProc();
+
+SELECT * FROM PreReqList;
